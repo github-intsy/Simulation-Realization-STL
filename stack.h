a4126502355b18634a5926ba818ca853bddb5e26
gaoshengyu <https://github.com/github-intsy/study-code.git>
2023-04-28 18:40:55 +0800

    stack study01

---
+++ stack.h
@@ -0,0 +1,47 @@
+#pragma once
+namespace gsy_study_stack
+{
+	template<class T, class Container>
+	class stack
+	{
+	public:
+		void push(const T& val)
+		{
+			_con.push_back(val);
+		}
+		void pop()
+		{
+			_con.pop_back();
+		}
+		T& top()
+		{
+			return _con.back();
+		}
+		size_t size()
+		{
+			return _con.size();
+		}
+		bool empty()
+		{
+			return _con.empty();
+		}
+
+	private:
+		Container _con;
+	};
+	void test_stack()
+	{
+		stack<int, list<int>> st;
+		st.push(1);
+		st.push(2);
+		st.push(3);
+		st.push(4);
+		while (!st.empty())
+		{
+			cout << st.top() << ' ';
+			st.pop();
+		}
+		cout << endl;
+
+	}
+}
--- test.cpp
+++ test.cpp
@@ -3,28 +3,13 @@
 #include<string>
 #include<vector>
 #include<assert.h>
+#include<list>
 using namespace std;
 #include"list.h"
-<<<<<<< HEAD
 #include"string.h"
 #include"vector.h"
+#include"stack.h"
 int main()
 {
-	gsy_study_vector::test02();
-	gsy_study_list::Test();
-	gsy::Test02();
-	/*vector<int> v1;
-	v1.push_back(10);
-	v1.push_back(20);
-	vector<int>::iterator i = v1.begin();
-	v1.insert(v1.begin(), 100);
-	for (auto e : v1)
-		cout << e << ' ';*/
-=======
-int main()
-{
-	gsy_study_list::Test();
-	
->>>>>>> 6ed98de55e41b9487f85bd99f1b9df445e4b1416
-	return 0;
+	gsy_study_stack::test_stack();
 }
